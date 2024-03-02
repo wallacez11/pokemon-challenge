dd703f1be1d2d2594572881a6721684839088f76
wallacez11 <wallace.santos.mail@gmail.com>
2024-03-02 01:27:53 -0300

    validation body

--- app/auth.py
+++ app/auth.py
@@ -7,12 +7,18 @@ auth_bp = Blueprint('auth', __name__)
 @auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.json
+
     try:
         user = UserRequestBody(email=data["email"], password=data["password"])
-        print(user)
-    except ValueError  as e:
-        print(e.errors()[0]["msg"])
-    return jsonify(data)
+    except ValueError as e:
+        error_messages = []
+        for error in e.errors():
+            msg = error["msg"].split(", ", 1)[1]
+            error_messages.append(msg)
+        if error_messages:
+            return jsonify({'message': ', '.join(error_messages)}), 400
+
+    return jsonify(user)
    
 
 @auth_bp.route('/register', methods=['POST'])
