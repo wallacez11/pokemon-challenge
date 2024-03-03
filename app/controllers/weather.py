361e2bbf49bb3d1409109a79bda177a635b7e38d
wallacez11 <wallace.santos.mail@gmail.com>
2024-03-03 20:46:13 -0300

    logging weather functions

--- app/controllers/weather.py
+++ app/controllers/weather.py
@@ -1,4 +1,5 @@
 import requests
+from flask import current_app
 import openmeteo_requests
 import requests_cache
 from retry_requests import retry
@@ -9,28 +10,38 @@ openmeteo = openmeteo_requests.Client(session = retry_session)
 
 ## getting current latitude and longitude
 def get_lat_lng():
-    url = "https://ipinfo.io/json"
-    response = requests.get(url)
-    data = response.json()
-    lat_lng = data["loc"].split(",")
-    latitude = float(lat_lng[0])
-    longitude = float(lat_lng[1])
-    return latitude, longitude
+    try:
+        url = "https://ipinfo.io/json"
+        response = requests.get(url)
+        data = response.json()
+        lat_lng = data["loc"].split(",")
+        latitude = float(lat_lng[0])
+        longitude = float(lat_lng[1])
+        current_app.logger.info('Retrieved current latitude and longitude')
+        return latitude, longitude
+    except Exception as e:
+        current_app.logger.error(f'Error retrieving latitude and longitude: {e}')
+        return None, None
 
 ## getting info weather based on the latitude and longitude
 def get_current_weather():
-    latitude, longitude = get_lat_lng()
-    url = "https://api.open-meteo.com/v1/forecast"
-    params = {
-        "latitude": latitude,
-        "longitude": longitude,
-        "current": "temperature_2m"
-    }
-    responses = openmeteo.weather_api(url, params=params)
-    response = responses[0]
-    current = response.Current()
-    current_temperature = current.Variables(0).Value()
-    return current_temperature
+    try:
+        latitude, longitude = get_lat_lng()
+        url = "https://api.open-meteo.com/v1/forecast"
+        params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "current": "temperature_2m"
+        }
+        responses = openmeteo.weather_api(url, params=params)
+        response = responses[0]
+        current = response.Current()
+        current_temperature = current.Variables(0).Value()
+        current_app.logger.info(f'Retrieved current temperature: {current_temperature}')
+        return current_temperature
+    except Exception as e:
+        current_app.logger.error(f'Error retrieving current weather: {e}')
+        return None
 
 def get_pokemon_type_by_temperature():
     temperature = get_current_weather()
